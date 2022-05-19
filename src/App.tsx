d0cd3acac4a6c601f6fb0947980b6f6cc7037e13
dddarthas <daniel.focsa14>
2022-05-19 20:07:13 +0300

    lab5

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,28 @@
-
 import React from 'react';
 import './App.css';
-import Layout_content from "./components/Layout"
-import {StudenOverview} from "./components/Overview"
+import LayoutContent from "./components/Layout";
+import {Overview} from "./components/Overview";
+import rootStore from './mst/Provider.store';
 
-
-const infoStudent = [
-  {
-    firstName: "Focsa",
-    secondName:"Daniel",
-    gender:"male",
-    age:21,
-    height:200,
-    univerity: "UTM",
-    speciality:"RM",
-  },
-  {
-    firstName: "Rusnac",
-    secondName:"Sandu",
-    gender:"male",
-    age:13,
-    height:30,
-    univerity: "Hogwarts",
-    speciality:"Slytherin",
-
-  },
-  {
-    firstName: "Jimbei",
-    secondName:"Alexandru",
-    gender:"male",
-    age:22,
-    height:180,
-    univerity: "UTM",
-    speciality:"RM",
-  }
-]
 
 function App() {
+  function refreshPage() {
+    window.location.reload();
+  }
+
   return (
     <div className="App">
-        <Layout_content
+        <LayoutContent
         />
-        <StudenOverview 
-        students = {infoStudent}
+        <Overview students={rootStore.students}
         />
+        <button onClick={rootStore.saveToLocalStorage}>Save to LocalStorage</button>
+        <br />
+        <button onClick={rootStore.clearLocalStorage}>Clear LocalStorage</button>
+        <br />
+        <button onClick={rootStore.setLocalStorageTestData}>Set LocalStorage test data</button>
+        <br />
+        <button onClick={refreshPage}>Click to reload!</button>
     </div>
   );
 }
